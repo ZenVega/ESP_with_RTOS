5292bee27f48161cea4504fcf38ceffcc1f03d17
zen_vega <ufogreeks@gmail.com>
2025-07-27 22:26:09 +0200

    feat: task controls and TickCount

--- main/main_rtos_tasks.c
+++ main/main_rtos_tasks.c
@@ -13,14 +13,35 @@
 //                         TaskHandle_t *pxCreatedTask					--pointer to task for later referencing
 //                       );
 
+TaskHandle_t task_1_handle = NULL;
+TaskHandle_t task_2_handle = NULL;
+
 void	task_1(void *arg)
 {
 	while(1)
 	{
-		printf("hello from task 1\n");
+		int i = xTaskGetTickCount() - 1;
+		printf("[%d] hello from task 1\n", i);
 		// both delays last 500ms
-		vTaskDelay(500/portTICK_PERIOD_MS);
-		vTaskDelay(pdMS_TO_TICKS(500));
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+		if (i == 300)
+		{
+			//must only be called once on a running task. else core panic'd
+			vTaskSuspend(task_2_handle);
+			printf("task 2 gets suspended. StateID: %d\n", eTaskGetState(task_2_handle));
+		}
+		if (i == 500)
+		{
+			//must only be called once on a running task. else core panic'd
+			vTaskResume(task_2_handle);
+			printf("task 2 resumes. StateID: %d\n", eTaskGetState(task_2_handle));
+		}
+		if (i == 700)
+		{
+			//must only be called once on a running task. else core panic'd
+			vTaskDelete(task_2_handle);
+			printf("task 2 deleted by task 1. StateID: %d\n", eTaskGetState(task_2_handle));
+		}
 	}
 }
 
@@ -33,8 +54,6 @@ void	task_2(void *arg)
 		vTaskDelay(pdMS_TO_TICKS(1000));
 	}
 }
-TaskHandle_t task_1_handle = NULL;
-TaskHandle_t task_2_handle = NULL;
 
 void app_main()
 {
@@ -42,4 +61,4 @@ void app_main()
 	//last argument xCoreID can set this task to a defined core
 	xTaskCreatePinnedToCore( task_2, "task2", 4096, NULL, 10, &task_2_handle, 1);
 }
-
+//NEXT: https://www.youtube.com/watch?v=VkCvKtRsunU
